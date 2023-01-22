87ed3a7574005e9f8998c0fe1739ff47b4abf3fc
Bilostenko <bilostenkod@gmail.com>
2023-01-22 21:28:39 +0200

    add js radio

--- main.js
+++ main.js
@@ -6,47 +6,72 @@ let redKettle = document.querySelector("#red")
 let pinkKettle = document.querySelector("#pink")
 let beigeKettle = document.querySelector("#beige")
 /* radio buttons*/
+let option1 = document.querySelector("#option1")
+let option2 = document.querySelector("#option2")
+let option3 = document.querySelector("#option3")
+let option4 = document.querySelector("#option4")
 option1.checked = true;
-
 
 blueKettle.onclick = () => {
   bcgrnd.style.backgroundColor = "#95CFD5"
   document.querySelector(".active").classList.remove("active")
-  blueKettle.classList.add("active") 
+  blueKettle.classList.add("active")
   bigKettle.src = "./img/kettle-bg/blue-kettle.png";
-
   document.querySelector('input').checked = false
   option1.checked = true
 }
+option1.onclick = () => {
+  bcgrnd.style.backgroundColor = "#95CFD5"
+  document.querySelector(".active").classList.remove("active")
+  blueKettle.classList.add("active")
+  bigKettle.src = "./img/kettle-bg/blue-kettle.png";
+
+}
+
 
 redKettle.onclick = () => {
   bcgrnd.style.backgroundColor = "red"
   document.querySelector(".active").classList.remove("active")
-  redKettle.classList.add("active")   
+  redKettle.classList.add("active")
   bigKettle.src = "./img/kettle-bg/red-kettle.png";
-
   document.querySelector('input').checked = false
   option2.checked = true
- 
+}
+option2.onclick = () => {
+  bcgrnd.style.backgroundColor = "red"
+  document.querySelector(".active").classList.remove("active")
+  redKettle.classList.add("active")
+  bigKettle.src = "./img/kettle-bg/red-kettle.png";
 }
 
-pinkKettle.onclick = () =>{
+
+pinkKettle.onclick = () => {
   bcgrnd.style.backgroundColor = "pink"
   document.querySelector(".active").classList.remove("active")
-  pinkKettle.classList.add("active")  
+  pinkKettle.classList.add("active")
   bigKettle.src = "./img/kettle-bg/pink-kettle.png";
-
   document.querySelector('input').checked = false
   option3.checked = true
 }
+option3.onclick = () => {
+  bcgrnd.style.backgroundColor = "pink"
+  document.querySelector(".active").classList.remove("active")
+  pinkKettle.classList.add("active")
+  bigKettle.src = "./img/kettle-bg/pink-kettle.png";
+}
+
 
 beigeKettle.onclick = () => {
   bcgrnd.style.backgroundColor = "beige"
   document.querySelector(".active").classList.remove("active")
-  beigeKettle.classList.add("active")  
+  beigeKettle.classList.add("active")
   bigKettle.src = "./img/kettle-bg/beige-kettle.png";
-
   document.querySelector('input').checked = false
   option4.checked = true
 }
-
+option4.onclick = () => {
+  bcgrnd.style.backgroundColor = "beige"
+  document.querySelector(".active").classList.remove("active")
+  beigeKettle.classList.add("active")
+  bigKettle.src = "./img/kettle-bg/beige-kettle.png";
+}
